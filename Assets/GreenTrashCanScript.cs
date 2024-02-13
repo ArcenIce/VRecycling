e49ea815d74d59a863c2f05c7b94b0bc0d721f0a
Pierre Crepin <95847387+Pierrecrp1@users.noreply.github.com>
2024-02-13 16:51:52 +0100

    toutes les poubelles

--- Assets/GreenTrashCanScript.cs
+++ Assets/GreenTrashCanScript.cs
@@ -1,12 +1,12 @@
 using UnityEngine;
 
-public class CollisionGameObjectExample : MonoBehaviour
+public class GreenTrashCanCollision : MonoBehaviour
 {
     //Detect collisions between the GameObjects with Colliders attached
     void OnCollisionEnter(Collision collision)
     {
         //Check for a match with the specified name on any GameObject that collides with your GameObject
-        if (collision.gameObject.tag == "BlueTrash")
+        if (collision.gameObject.tag == "GreenTrash")
         {
             //If the GameObject's name matches the one you suggest, output this message in the console
             Debug.Log("C'est le bon tag");
---
+++ Assets/YellowTrashCanScript.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class YellowTrashCanCollision : MonoBehaviour
+{
+    //Detect collisions between the GameObjects with Colliders attached
+    void OnCollisionEnter(Collision collision)
+    {
+        //Check for a match with the specified name on any GameObject that collides with your GameObject
+        if (collision.gameObject.tag == "YellowTrash")
+        {
+            //If the GameObject's name matches the one you suggest, output this message in the console
+            Debug.Log("C'est le bon tag");
+        }
+        else
+        {
+            Debug.Log("Mauvais tag");
+        }
+        Destroy(collision.gameObject);
+    }
+}
